d54b7fddd83adcc99d6c7c35d7217020fb90c17c
Jonas A. Hultén <jonas@sldr.se>
2020-10-03 18:46:37 +0200

    Added the first wordlist
    Put it in a separate file to not flood the business logic with
    strings

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+mod wordlist;
+
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 
-const WORD: &[&str] = &["foo", "bar", "baz", "quz", "no"];
-
-fn random_word(rng: &mut ThreadRng) -> String {
-    WORD.choose(rng).unwrap().to_string()
+fn random_pluralnoun(rng: &mut ThreadRng) -> String {
+    wordlist::PLURALNOUN.choose(rng).unwrap().to_string()
 }
 
 fn main() {
     let mut rng: ThreadRng = rand::thread_rng();
     println!("Hello, world!");
-    println!("Our random word is {}", random_word(&mut rng));
-    println!("Our next random word is {}", random_word(&mut rng));
+    println!("Our random word is {}", random_pluralnoun(&mut rng));
+    println!("Our next random word is {}", random_pluralnoun(&mut rng));
 }
