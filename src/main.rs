124e79dd19802100ba7a612cca977ebb66759d53
Jonas A. Hultén <jonas@sldr.se>
2020-10-03 17:55:42 +0200

    PoC pick a random word implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::seq::SliceRandom;
+
+fn random_word() -> String {
+    let mut rng = rand::thread_rng();
+    let words: [&str; 3] = ["foo", "bar", "baz"];
+    words.choose(&mut rng).unwrap().to_string()
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("Our random word is {}", random_word());
 }
