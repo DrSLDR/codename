963638711bace94ef699b58ae80c028b9461d218
Jonas A. Hultén <jonas@sldr.se>
2020-10-03 18:30:49 +0200

    Refactored to allow a shared RNG
    I have no idea if this is more efficient, but it feels like it

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 
 const WORD: &[&str] = &["foo", "bar", "baz", "quz", "no"];
 
-
-fn random_word() -> String {
-    let mut rng = rand::thread_rng();
-    WORD.choose(&mut rng).unwrap().to_string()
+fn random_word(rng: &mut ThreadRng) -> String {
+    WORD.choose(rng).unwrap().to_string()
 }
 
 fn main() {
+    let mut rng: ThreadRng = rand::thread_rng();
     println!("Hello, world!");
-    println!("Our random word is {}", random_word());
+    println!("Our random word is {}", random_word(&mut rng));
+    println!("Our next random word is {}", random_word(&mut rng));
 }
