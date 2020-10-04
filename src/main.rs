03133f7b45a2ba1d9ff3016f16f1c2e0e78bb96b
Jonas A. Hultén <jonas@sldr.se>
2020-10-04 16:26:43 +0200

    Added the corresponding functions

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,26 @@ use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 use wordlist as wl;
 
+fn random_adjective(rng: &mut ThreadRng) -> String {
+    wl::ADJECTIVE.choose(rng).unwrap().to_string()
+}
+
+fn random_adverb(rng: &mut ThreadRng) -> String {
+    wl::ADVERB.choose(rng).unwrap().to_string()
+}
+
 fn random_pluralnoun(rng: &mut ThreadRng) -> String {
     wl::PLURALNOUN.choose(rng).unwrap().to_string()
 }
 
+fn random_verb(rng: &mut ThreadRng) -> String {
+    wl::VERB.choose(rng).unwrap().to_string()
+}
+
 fn main() {
     let mut rng: ThreadRng = rand::thread_rng();
-    println!("Hello, world!");
-    println!("Our random word is {}", random_pluralnoun(&mut rng));
-    println!("Our next random word is {}", random_pluralnoun(&mut rng));
+    println!("Random adjective: {}", random_adjective(&mut rng));
+    println!("Random adverb: {}", random_adverb(&mut rng));
+    println!("Random pluralnoun: {}", random_pluralnoun(&mut rng));
+    println!("Random verb: {}", random_verb(&mut rng));
 }
