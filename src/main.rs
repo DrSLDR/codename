2d748e0ca2aab4c56833d8db1898cc96409c8f55
Jonas A. Hultén <jonas@sldr.se>
2020-10-04 19:45:51 +0200

    Replaced the list of classes with a map
    This should allow me to iterate over the keys and also retrieve
    the lists while I'm doing the querying.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ mod wordlist;
 
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
+use std::collections::HashMap;
 use wordlist as wl;
-
-const CLASS: &[&str] = &[
-    "#ADJECTIVE#",
-    "#ADVERB#",
-    "#PLURALNOUN#",
-    "#VERB#",
-];
 
 const PATTERN: &[&str] = &[
     "#ADJECTIVE##PLURALNOUN##VERB##ADVERB#",
 ];
+
+fn generate_classmap() -> HashMap<String, &'static[&'static str]> {
+    let mut map: HashMap<String, &[&str]> = HashMap::new();
+    map.insert("#ADJECTIVE#".to_string(), wl::ADJECTIVE);
+    map.insert("#ADVERB#".to_string(), wl::ADVERB);
+    map.insert("#PLURALNOUN#".to_string(), wl::PLURALNOUN);
+    map.insert("#VERB#".to_string(), wl::VERB);
+    map
+}
 
 fn random_adjective(rng: &mut ThreadRng) -> String {
     wl::ADJECTIVE.choose(rng).unwrap().to_string()
@@ -33,6 +36,8 @@ fn random_verb(rng: &mut ThreadRng) -> String {
 
 fn main() {
     let mut rng: ThreadRng = rand::thread_rng();
+    let map = generate_classmap();
+
     println!("Random adjective: {}", random_adjective(&mut rng));
     println!("Random adverb: {}", random_adverb(&mut rng));
     println!("Random pluralnoun: {}", random_pluralnoun(&mut rng));
